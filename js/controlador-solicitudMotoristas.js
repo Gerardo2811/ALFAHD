fa5f86d207ef47713d1cee6d0c18512bf170604a
Emanuel Espinoza <emanuel070801@gmail.com>
2022-04-24 19:11:22 -0600

    cambios

--- js/controlador-solicitudMotoristas.js
+++ js/controlador-solicitudMotoristas.js
@@ -19,13 +19,13 @@ function llenarTablaMotoristas(){
     document.querySelector('#tabla-motoristas tbody').innerHTML ='';
     for(let i=0; i<motoristas.length;i++){
     document.querySelector('#tabla-motoristas tbody').innerHTML +=
-    `<tr>
+    `<tr id=${i}>
         <td id="nombre-motorista">${motoristas[i].nombreCompleto}</td>
         <td id="correo-motorista">${motoristas[i].correoElectronico}</td>
         <td id="fecha-motorista">${motoristas[i].fechaNacimiento}</td>
         <td id="departamento-motorista">${motoristas[i].departamentoLaboral}</td>
         <td>
-        <button type="button" onclick="guardarMotoristaOficial(${i})">Aceptar</button>
+        <button type="button" onclick="aceptarMotorista(${i})">Aceptar</button>
         <button type="button" onclick="eliminarMotorista(${i})">Rechazar</button>
         </td>
     </tr>` ;
@@ -41,32 +41,35 @@ function eliminarMotorista(indice){
         responseType:'json'
         }) .then(res=>{
             console.log(res.data);
-           obtenerUsuarios();
         }) .catch(error=>{
             console.error(error);
         });
 }
 
-function guardarMotoristaOficial(){
+function aceptarMotorista(indice){
+    document.getElementById(indice).style.display = "none";
+}
 
-    motoristaOficial ={
-        nombreCompleto:document.getElementById('nombre-motorista').value,
-        correoElectronico:document.getElementById('correo-motorista').value,
-        fechaNacimiento:document.getElementById('fecha-motorista').value,
-        departamentoLaboral:document.getElementById('departamento-motorista').value,
-    };
-    axios({
-        method:'POST',
-        url:'../ALFHAD/api/motorista-oficiales.php',
-        responseType:'json',
-        data: motorista
-        }) .then(res=>{
-            console.log(res.data);
-            this.motorista=res.data;
-        }) .catch(error=>{
-            console.error(error);
-        });
+// function guardarMotoristaOficial(){
+
+//     motoristaOficial ={
+//         nombreCompleto:document.getElementById('nombre-motorista').value,
+//         correoElectronico:document.getElementById('correo-motorista').value,
+//         fechaNacimiento:document.getElementById('fecha-motorista').value,
+//         departamentoLaboral:document.getElementById('departamento-motorista').value,
+//     };
+//     axios({
+//         method:'POST',
+//         url:'../ALFHAD/api/motorista-oficiales.php',
+//         responseType:'json',
+//         data: motorista
+//         }) .then(res=>{
+//             console.log(res.data);
+//             this.motorista=res.data;
+//         }) .catch(error=>{
+//             console.error(error);
+//         });
        
 
 
-}
+// }
